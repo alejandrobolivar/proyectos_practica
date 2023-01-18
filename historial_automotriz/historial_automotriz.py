fec7c2a5041239e717335f808e61ca32f7353f41
alejandrobolivar <bolivara@gmail.com>
2023-01-18 17:11:50 -0400

    enunciado1

--- historial_automotriz/historial_automotriz.py
+++ historial_automotriz/historial_automotriz.py
@@ -1,4 +1,12 @@
 '''
-Registro de mantenimiento del vehículo.
+Registro de mantenimiento de un vehículo.
+En un archivo "historial_de_mantenimiento.txt", se lleva el registro de las actividades de mantenimiento realizadas al vehículo, los datos almacenados son:
+
+marca, modelo, placa, año
+fecha, kilometraje, mantenimiento, costo, fecha_proxima_cita
+
+Estos datos son introducidos por teclado y almacenados en el archivo "historial_de_mantenimiento.txt".
+
+Crear un archivo "por_mantenimiento.txt" con los mantenimientos con cita vencida (fecha actual mayor a la fecha_proxima_cita) que contenga: mantenimiento, costo_estimado (es el mayor de los costos del mantenimiento) y al final el total estimado en mantenimiento.
 
 '''
