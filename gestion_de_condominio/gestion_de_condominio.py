b070983d8f2d61242e5134a3f8eb053f782cdf42
Alejandro Bolívar <bolivara@gmail.com>
2023-04-16 16:48:20 -0400

    Update gestion_de_condominio.py

--- gestion_de_condominio/gestion_de_condominio.py
+++ gestion_de_condominio/gestion_de_condominio.py
@@ -1,4 +1,6 @@
 '''
 Gestiona los ingreso y egresos en la gestión del condominio.
 
+Proyecto del equipo de participantes del Curso de Programación en Python. En este proyecto efectuaremos una demostración de lo aprendido en el curso de programación en Python. El objetivo del proyecto es crear un programa en python que permita la gestión administrativa en un condominio. A continuación algunas especificaciones que pueden ser discutibles durante el desarrollo del proyecto. 1.Desarrollar las clases pertinentes para administrar la información de: personas familia unidad habitacional agrupación de unidades desarrollo habitacional general. 2.Desarrollar los métodos necesarios para la gestión de los objetos. 3.Control de Gastos Fijos 4.Control de Gastos Variables 5.Control de Ingresos 6.Facturación 7.Otros
+
 '''
